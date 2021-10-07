6251b777f2d97875a7b343df90de3bc14aab5656
SomeRandomCloud <samuel.loddo@gmail.com>
2021-10-07 14:54:45 +0200

    e

--- events/ready.js
+++ events/ready.js
@@ -2,16 +2,9 @@ module.exports = {
 	name: 'ready',
 	once: true,
 	async execute(client) {
-
-		// ──────────────────────────────────────────────────────────────────── [ Sets the bots activity ]
-
 		client.user.setActivity('a Trolling Tournament!', { type: 'COMPETING' });
-
-		// ────────────────────────────────────────────────────────────────────  [ Adds a little indicator to indicate the bot is ready! (Runs for 30 seconds) ]
 
 		console.log('The bot has awakened');
 		console.log(`Logged in as: ${client.user.tag}`);
-
-		// ────────────────────────────────────────────────────────────────────
 	},
 };
